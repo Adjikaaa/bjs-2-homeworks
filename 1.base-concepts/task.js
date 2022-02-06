e4a6f22a51a04588f95ec73a468be316c76458d4
Mariya Kokhonova <adjika88@gmail.com>
2022-02-06 21:10:27 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,15 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  	  'use strict'
+		let D = Math.pow(b,2)-4*a*c;
+		let solution = [];
+		if (D==0) {
+		  solution.push(-b/(2*a));
+		} else if (D>0) {
+		  solution.push((((-b-Math.sqrt(D))/(2*a))).toFixed(2));
+		  solution.push((((-b+Math.sqrt(D))/(2*a))).toFixed(2));
+		} 
+		return solution;
+	
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
