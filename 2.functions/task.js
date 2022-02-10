6a26ba955a4dcc80c1ece8edaa5fbb00f290b028
Mariya Kokhonova <adjika88@gmail.com>
2022-02-10 14:56:02 +0300

    Changes for Function task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -7,18 +7,14 @@ function getArrayParams([...arr]) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] < min) {
             min = arr[i];
-        } else {
-            min = min;
         }
         if (arr[i] > max) {
             max = arr[i];
-        } else {
-            max = max;
         }
         sum += arr[i];
+    }
         avg = (sum / arr.length).toFixed(2);
         avg = Number(avg);
-    }
     return {
         min: min,
         max: max,
@@ -36,46 +32,18 @@ function worker([...arr]) {
 }
 
 function makeWork(arrOfArr, func) {
-    let max = worker(arrOfArr[0]);
+    let max = func(arrOfArr[0]);
     for (let i = 0; i < arrOfArr.length; i++) {
-        if (func(arrOfArr[i]) > max) {
-            max = func(arrOfArr[i]);
-        } else {
-            max = max;
-        }
+      let resultOfFunc = func(arrOfArr[i]);
+         if (resultOfFunc > max) {
+         max = resultOfFunc;
+         }
     }
     return max;
 }
 
 // Задание 3
 function worker2([...arr]) {
-    let min = arr[0];
-    let max = arr[0];
-    let delta = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (min < arr[i]) {
-            min = min;
-        } else {
-            min = arr[i]
-        }
-        if (max > arr[i]) {
-            max = max;
-        } else {
-            max = arr[i]
-        }
-    }
-    delta = Math.abs(max - min);
-    return delta;
-}
-
-function makeWork(arrOfArr, func) {
-    let max = worker2(arrOfArr[0]);
-    for (let i = 0; i < arrOfArr.length; i++) {
-        if (func(arrOfArr[i]) > max) {
-            max = func(arrOfArr[i]);
-        } else {
-            max = max;
-        }
-    }
-    return max;
-}
+  let arrParams = getArrayParams(arr);
+  return Math.abs(arrParams.max - arrParams.min);
+};
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
-function compareArrays(arr1, arr2) {
-  let result;
+function compareArrays([arr1], [arr2]) {
+  let result = true;
+  if (array1.length === array2.length)
+        return array1.every((a, index) => a === array2[index])
+    else
+        return false
+}
 
-  // Ваш код
 
   return result; // boolean
 }
